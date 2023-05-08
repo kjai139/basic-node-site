d4d53028320f078bef439109db91cd071914d3cb
simon y <71997308+kjai139@users.noreply.github.com>
2023-05-08 18:21:39 -0400

    updated with express

--- index.js
+++ index.js
@@ -1,44 +1,71 @@
 const http = require('http')
 const fs = require('fs')
 const path = require('path')
+const express = require('express')
+const app = express()
 
-const server = http.createServer((request, res) => {
-    const url = request.url
 
-    
 
-    // let filePath = '.' + path
-    // if (filePath === './') {
-    //     filePath = './index.html'
-    // } else {
-    //     filePath = '.' + filePath + 'html'
-    // }
-    // console.log(filePath, 'file path')
-
-    let filePath = path.join(__dirname, `${url}.html`)
-    if (url === '/') {
-        filePath = './index.html'
-    }
-
-    fs.access(filePath, fs.F_OK, (err) => {
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html', (err) => {
         if (err) {
-            res.writeHead(404)
-            res.end('404 not found')
-            return
+            console.log('error sending home', err)
+        } else {
+            console.log('file sent')
         }
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
-                res.writeHead(500)
-                res.end('500 internal server error')
-                return
-            }
-
-            res.writeHead(200)
-            res.end(data)
-        })
     })
 })
 
-server.listen(8080, () => {
+//when using sendFile it auto set headers and end response so no need to use res.end or set status codes
+
+app.get('/:name', (req, res)=> {
+    const fileName = req.params.name
+    // console.log(fileName)
+    const url = req.url
+    // console.log(url)
+
+    const filePath = __dirname + `${url}.html`
+
+    res.sendFile(filePath, function(err) {
+        if (err) {
+            console.log('error sending', err)
+        } else {
+            console.log('file sent', filePath)
+            console.log('file name:', fileName)
+            console.log('url name:', url)
+        }
+    })
+})
+
+
+// const server = http.createServer((request, res) => {
+//     const url = request.url
+
+
+//     let filePath = path.join(__dirname, `${url}.html`)
+//     if (url === '/') {
+//         filePath = './index.html'
+//     }
+
+//     fs.access(filePath, fs.F_OK, (err) => {
+//         if (err) {
+//             res.writeHead(404)
+//             res.end('404 not found')
+//             return
+//         }
+//         fs.readFile(filePath, (err, data) => {
+//             if (err) {
+//                 res.writeHead(500)
+//                 res.end('500 internal server error')
+//                 return
+//             }
+
+//             res.writeHead(200)
+//             res.end(data)
+//         })
+//     })
+// })
+
+app.listen(8080, () => {
     console.log('server running on port 8080')
 })
